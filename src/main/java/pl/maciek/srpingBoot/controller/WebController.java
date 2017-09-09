0909e00577be7f6b99e2919b2f59a8e56c9bb9c1
Gemm <gemelek@o2.pl>
2017-09-09 17:23:43 +0100

    add request

--- src/main/java/pl/maciek/srpingBoot/controller/WebController.java
+++ src/main/java/pl/maciek/srpingBoot/controller/WebController.java
@@ -1,10 +1,15 @@
 package pl.maciek.srpingBoot.controller;
 
+import org.apache.log4j.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import pl.maciek.srpingBoot.model.Article;
+
+import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
 
 /**
  * Created by macie on 26.08.2017.
@@ -31,9 +36,16 @@ public class WebController  {
         return "article/add";
     }
 
+//    @GetMapping("/show")
+//    public String show(@ModelAttribute Article article, ModelMap modelMap){
+//        modelMap.addAttribute("article", article);
+//        return "article/show";
+//    }
+
     @GetMapping("/show")
-    public String show(@ModelAttribute Article article, ModelMap modelMap){
-        modelMap.addAttribute("article", article);
+    public String show(@RequestParam ("city") String city, ModelMap modelMap){
+        modelMap.addAttribute("city", city);
+        System.out.println(city);
         return "article/show";
     }
 }
